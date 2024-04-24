08440588613fc504b120d03e464f6ecc1d63f4f2
mairarafiq301 <mairarafiq301@gmail.com>
2024-04-24 01:12:01 -0700

    push arrow function code to github

---
+++ app.js
@@ -0,0 +1,3 @@
+var tea = function (milk, sugar, tealeaves) { return (milk + sugar + tealeaves); };
+var dudhpatti = tea(1, 3, 4);
+console.log(dudhpatti);
---
+++ app.ts
@@ -0,0 +1,6 @@
+let tea = (milk:number , sugar:number , tealeaves: number) => (
+    milk + sugar +tealeaves
+)
+
+let dudhpatti:number = tea(1,3,4)
+console.log(dudhpatti)
