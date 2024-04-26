2c8076584ee8d2a2910774e025c6769cf0632fe0
mairarafiq301 <mairarafiq301@gmail.com>
2024-04-26 04:09:44 -0700

    first commit

--- app.js
+++ app.js
@@ -1,3 +1,39 @@
-var tea = function (milk, sugar, tealeaves) { return (milk + sugar + tealeaves); };
-var dudhpatti = tea(1, 3, 4);
-console.log(dudhpatti);
+/*let tea = (milk:number , sugar:number , tealeaves: number) => (
+    milk + sugar +tealeaves
+)
+
+let dudhpatti:number = tea(1,3,4)
+console.log(dudhpatti) */
+//default parameter
+var friend = function (name, age) {
+    if (age === void 0) { age = 20; }
+    console.log("My friend name is ".concat(name, " her age is ").concat(age, "\nand she is very pretty MASHALLAH. "));
+};
+friend("maira", 15);
+friend("ZARAH", 2);
+friend("Sana");
+// rest in arrow function
+var friend1 = function (name, age) {
+    if (age === void 0) { age = 20; }
+    var rest = [];
+    for (var _i = 2; _i < arguments.length; _i++) {
+        rest[_i - 2] = arguments[_i];
+    }
+    return console.log("hello" + "My friend name is ".concat(name, " her age is ").concat(age, "\nand she is very pretty MASHALLAH.") + rest);
+};
+friend1("maira", 15, "clifton");
+friend1("ZARAH", 2, "DHA");
+friend1("Sana");
+// return in arrow function
+var friend2 = function (name, age) {
+    if (age === void 0) { age = 20; }
+    return "My friend name is ".concat(name, " her age is ").concat(age, "\nand she is very pretty MASHALLAH. ");
+};
+console.log(friend2("maira", 15));
+console.log(friend2("ZARAH", 2));
+console.log(friend2("Sana"));
+var calculator = function (num1, num2) {
+    var result = num1 - num2;
+    return result;
+};
+console.log(calculator(3, 6));
--- app.ts
+++ app.ts
@@ -1,6 +1,36 @@
-let tea = (milk:number , sugar:number , tealeaves: number) => (
+/*let tea = (milk:number , sugar:number , tealeaves: number) => (
     milk + sugar +tealeaves
 )
 
 let dudhpatti:number = tea(1,3,4)
-console.log(dudhpatti)
+console.log(dudhpatti) */
+//default parameter
+let friend=(name:string,age:number=20) =>{console.log(`My friend name is ${name} her age is ${age}
+and she is very pretty MASHALLAH. `)
+}
+friend("maira",15)
+friend("ZARAH",2)
+friend("Sana")    
+// rest in arrow function
+let friend1=(name:string,age:number=20,...rest:any[]) =>console.log("hello"+`My friend name is ${name} her age is ${age}
+and she is very pretty MASHALLAH.`+ rest)
+
+friend1("maira",15,"clifton")
+friend1("ZARAH",2,"DHA")
+friend1("Sana") 
+
+// return in arrow function
+
+let friend2=(name:string,age:number=20) =>{ return`My friend name is ${name} her age is ${age}
+and she is very pretty MASHALLAH. ` 
+
+}
+console.log(friend2("maira",15))
+console.log(friend2("ZARAH",2))
+console.log(friend2("Sana"))   
+
+let calculator = ( num1:number, num2: number)=>{
+       let result = num1 - num2
+       return result
+}
+console.log(calculator(3,6))
